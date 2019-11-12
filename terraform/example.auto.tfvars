2b31f51f4e280d5b08593f89c892879d85a1b5a6
Georgi Berchev <g.berchev@gmail.com>
2019-11-12 10:27:11 +0200

    add files

---
+++ terraform/example.auto.tfvars
@@ -0,0 +1,19 @@
+domain = "technology:collectors"
+
+environment = "prod"
+
+service_name = "test"
+
+db_ec2_ami = "ami-04b9e92b5572fa0d1" // Ubuntu 18.04 LTS
+
+db_ebs_data_volume_size = 200
+
+db_ec2_instance_type = "t3.small"
+
+key_name = "berchev_key_pair"
+
+db_username = "EXAMPLE_USERNAME"
+
+db_password = "EXAMPLE_PASSWORD"
+
+dd_api_key = "EXAMPLE_API_KEY"
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,44 @@
+# Using a single workspace:
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "berchevorg"
 
-    workspaces {
-      name = "filesystem-error"
+   workspaces {
+      name = "filesystem-error" # workspace qualifier
     }
   }
 }
 
-resource "null_resource" "test" {
-    triggers = {
-        build_number = "${timestamp()}"
-    }
+provider "aws" {
+  region  = "us-east-1"
+  profile = replace(var.environment, "stage", "dev") # remaps stage to use dev network
 }
 
-resource "random_pet" "name" {
- length    = "8"
- separator = "-"
+locals {
+  full_service_name = "${var.service_name}-${var.environment}"
+
+  tags = {
+    environment  = var.environment
+    terraform    = true
+    domain       = var.domain
+    service_name = var.service_name
+  }
 }
+
+# Load VPC Data Source
+#data "terraform_remote_state" "vpc" {
+#  backend = "remote"
+
+#  config = {
+#    organization = "deo"
+#    workspaces = {
+#      name = "networking-${replace(var.environment, "stage", "dev")}" # remaps stage to use dev network
+#    }
+#  }
+#}
+
+# Get current account id that terraform is running under
+data "aws_caller_identity" "current" {
+}
+
